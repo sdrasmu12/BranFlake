0afc3ef8a4e2637b12741c417d33a67dd928e230
sdrasmu12 <sdrasmu12@earlham.edu>
2016-08-13 23:20:17 -0500

    Fixed error due to commented out asteroids

--- common/defines/RPG_defines.lua
+++
@@ -1,4 +0,0 @@
-NDefines.NPop.GROWTH_BASE = 250	-- Required growth for a pop to become fully grown.
-NDefines.NPop.GROWTH_INCREASE_PER_POP = -23	-- Increased growth cost per pop planet [+ POP_INCREASE_PER_POP*(num_pop - 1)]
-NDefines.NPop.GROWTH_INCREASE_FACTOR = 1.8	-- Increased growth factor [+ (num_pop - 1)^POP_INCREASE_FACTOR]
-NDefines.NEconomy.COLONY_MONTHLY_GROWTH = 80
--- common/defines/Vickaris_defines.lua
+++ common/defines/Vickaris_defines.lua
@@ -1,2 +1,6 @@
 NDefines.NGameplay.TECH_COST_MULT_NUM_COLONIES				= 0 -- Each Planet adds +X% tech cost
 NDefines.NGameplay.TECH_COST_MULT_NUM_POPS					= 0 -- Each non-free Pop adds +X% tech cost
+NDefines.NPop.GROWTH_BASE = 250	-- Required growth for a pop to become fully grown.
+NDefines.NPop.GROWTH_INCREASE_PER_POP = -23	-- Increased growth cost per pop planet [+ POP_INCREASE_PER_POP*(num_pop - 1)]
+NDefines.NPop.GROWTH_INCREASE_FACTOR = 1.8	-- Increased growth factor [+ (num_pop - 1)^POP_INCREASE_FACTOR]
+NDefines.NEconomy.COLONY_MONTHLY_GROWTH = 80
